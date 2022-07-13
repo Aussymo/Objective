dd5e44a9e7e516abc00f64f6c4f298a5bdb11779
Austin Reed <97327532+Aussymo@users.noreply.github.com>
2022-07-13 12:16:09 -0600

    Created Repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from "react-bootstrap/Table";
+import React, { useState } from "react";
+import data from "./data.json";
 
-function App() {
+export const App = () => {
+  const [persons, setPersons] = useState(data.applicants);
+  const [jobs, setJobs] = useState(data.jobs);
+  const [skill, setSkill] = useState(data.skills);
+
+  const findJob = (id) => {
+    const result = jobs.find((item) => item.id === id);
+    // console.log(result);
+    return result.name;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table striped="columns">
+        <thead>
+          <tr>
+            <th>Job</th>
+            <th>Applicant Name</th>
+            <th>Email Address</th>
+            <th>Website</th>
+            <th>Skills</th>
+            <th>Cover Letter Paragraph</th>
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((person) => (
+            <tr>
+              {/* {console.log(person)} */}
+              <td>{findJob(person.job_id)}</td>
+              <td>{person.name}</td>
+              <td>{person.email}</td>
+              <td>{person.website}</td>
+              <td>
+                <table>
+                  <tbody>
+                    <tr>
+                      {skill.map((skill) => {
+                        if (skill.applicant_id === person.id) {
+                          return <td>{skill.name}</td>;
+                        }
+                      })}
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+
+              <td>{person.cover_letter}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
-}
-
-export default App;
+};
